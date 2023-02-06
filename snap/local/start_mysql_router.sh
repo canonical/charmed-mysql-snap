b81308320773d1e5617c0549a7c85b0de58dc98b
Jarred Wilson <jarred.wilson12@gmail.com>
2023-02-06 12:20:56 -0500

    Various script fixes

--- snap/local/start_mysql_router.sh
+++ snap/local/start_mysql_router.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# For security measures, daemons should not be run as sudo. Execute mongod as the non-sudo user: snap-daemon.
+# For security measures, daemons should not be run as sudo. Execute mysql as the non-sudo user: snap-daemon.
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
   --regid snap_daemon -- $SNAP/usr/bin/mysqlrouter "$@"
