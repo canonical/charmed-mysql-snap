6cc59dbd6168bfc5574108dee4da567896f3eb41
Carl Csaposs <carl.csaposs@canonical.com>
2023-06-15 18:01:07 +0000

    Run mysqlrouter and mysqlsh as non-root user

---
+++ snap/local/run-mysqlsh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# For security measures, applications should not be run as sudo. Execute mysqlsh as the non-sudo user: snap-daemon.
+exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+  --regid snap_daemon -- $SNAP/usr/bin/mysqlsh "$@"
