e22d70bae84681c0a17a16cf1ec6ac00d54dbe8a
Carl Csaposs <carl.csaposs@canonical.com>
2023-06-16 16:05:17 +0000

    Move mysqlsh logs to `/var/log/mysqlsh`

--- snap/local/run-mysqlsh.sh
+++ snap/local/run-mysqlsh.sh
@@ -2,4 +2,4 @@
 
 # For security measures, applications should not be run as sudo. Execute mysqlsh as the non-sudo user: snap-daemon.
 exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- $SNAP/usr/bin/mysqlsh "$@"
+  --regid snap_daemon -- $SNAP/usr/bin/mysqlsh --log-file /var/log/mysqlsh/ "$@"
