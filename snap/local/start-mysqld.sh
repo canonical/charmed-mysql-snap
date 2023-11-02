6cd064fea62d8f7cbfc4700ab8acb12970dbe08a
Shayan Patel <shayan.patel@canonical.com>
2023-11-02 13:46:24 +0000

    Clean up passing opts to commands + clean up comments

--- snap/local/start-mysqld.sh
+++ snap/local/start-mysqld.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# For security measures, applications should not be run as sudo.
+# Execute mysqld_safe as the non-sudo user: snap-daemon
+# Note: group is set to root due to backups related intricacies.
 exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
   --regid root -- $SNAP/usr/bin/mysqld_safe --defaults-file=$SNAP_DATA/etc/mysql/mysql.cnf "$@"
