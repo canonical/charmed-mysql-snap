99d4bc4e9982b795d9fd4c4f13719e575d80e4a9
Shayan Patel <shayan.patel@canonical.com>
2023-11-17 21:11:40 +0000

    Run mysqlrouter-passwd as a script that de-escalates privileges to snap_daemon

---
+++ snap/local/run-mysqlrouter-password.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# For security measures, applications should not be run as sudo.
+# Execute mysqlrouter as the non-sudo user: snap-daemon.
+exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
+  --regid snap_daemon -- $SNAP/usr/bin/mysqlrouter_passwd "$@"
