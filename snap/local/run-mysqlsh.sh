39ccef0919d69bed738136da561226ba1fe5c6fb
Paulo Machado <paulo.machado@canonical.com>
2024-01-18 21:12:32 -0300

    use snap log path

--- snap/local/run-mysqlsh.sh
+++ snap/local/run-mysqlsh.sh
@@ -3,4 +3,5 @@
 # For security measures, applications should not be run as sudo.
 # Execute mysqlsh as the non-sudo user: snap-daemon.
 exec $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
-  --regid snap_daemon -- env MYSQLSH_USER_CONFIG_HOME=/tmp/mysqlsh $SNAP/usr/bin/mysqlsh --log-file /var/log/mysqlsh/mysqlsh.log "$@"
+  --regid snap_daemon -- env MYSQLSH_USER_CONFIG_HOME=/tmp/mysqlsh $SNAP/usr/bin/mysqlsh \
+  --log-file $SNAP_COMMON/var/log/mysqlsh/mysqlsh.log "$@"
