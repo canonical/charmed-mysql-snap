4c4d5efe69c7459c208d9034e9e858c000163d3e
Vladyslav Tarasenko <vladislavstargate@gmail.com>
2025-01-23 10:49:32 +0200

    Fix start-mysql-pitr-helper-collector.sh for K8s.

--- snap/local/start-mysql-pitr-helper-collector.sh
+++ snap/local/start-mysql-pitr-helper-collector.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# For security measures, applications should not be run as sudo.
-# Execute mysqlsh as the non-sudo user: snap-daemon.
-exec "${SNAP}/usr/bin/setpriv" \
-  --clear-groups \
-  --reuid snap_daemon \
-  --regid snap_daemon \
-  -- \
-  "${SNAP}/usr/bin/mysql-pitr-helper" \
-  collect \
-  "${SNAP_DATA}/etc/mysql-pitr-helper-collector.yaml"
+if [ -z "$SNAP" ]; then
+    exec "/usr/bin/mysql-pitr-helper" \
+        collect \
+        "/etc/mysql-pitr-helper-collector.yaml"
+else
+    # For security measures, applications should not be run as sudo.
+    # Execute mysqlsh as the non-sudo user: snap-daemon.
+    exec "${SNAP}/usr/bin/setpriv" \
+        --clear-groups \
+        --reuid snap_daemon \
+        --regid snap_daemon \
+        -- \
+        "${SNAP}/usr/bin/mysql-pitr-helper" \
+        collect \
+        "${SNAP_DATA}/etc/mysql-pitr-helper-collector.yaml"
+fi
