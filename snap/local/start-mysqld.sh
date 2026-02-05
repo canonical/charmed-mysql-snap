88347c3d9ed3203f81878610b8495a421a781aac
Sinclert Pérez <sinclert.perez@canonical.com>
2026-02-05 10:50:25 +0100

    Replace mysqld by mysqld-safe binary

--- snap/local/start-mysqld.sh
+++ snap/local/start-mysqld.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # For security measures, applications should not be run as sudo.
-# Execute mysqld as the non-sudo user: snap-daemon
+# Execute mysqld_safe as the non-sudo user: snap-daemon
 # Note: group is set to root due to backups related intricacies.
 exec "${SNAP}/usr/bin/setpriv" \
     --clear-groups \
     --reuid snap_daemon \
     --regid root \
     -- \
-    "${SNAP}/usr/sbin/mysqld" --defaults-file="${SNAP_DATA}/etc/mysql/mysql.cnf"
+    "${SNAP}/usr/bin/mysqld_safe" --defaults-file="${SNAP_DATA}/etc/mysql/mysql.cnf"
