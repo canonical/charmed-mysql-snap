68d4ed5c127ca3e3a929450c54086d9efc11b8b9
Sinclert Pérez <sinclert.perez@canonical.com>
2026-02-09 11:35:13 +0100

    Touch mysqld_safe required files

--- snap/local/start-mysqld.sh
+++ snap/local/start-mysqld.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Touch mysqld_safe required log-files
+touch $SNAP_COMMON/var/log/mysql/error.log
+
 # For security measures, applications should not be run as sudo.
 # Execute mysqld_safe as the non-sudo user: snap-daemon
 # Note: group is set to root due to backups related intricacies.
