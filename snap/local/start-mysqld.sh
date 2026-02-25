ec036ce56b6dd3e6eded8b107b73a6f919fc8bb1
Sinclert Pérez <sinclert.perez@canonical.com>
2026-02-25 08:52:16 +0100

    [MISC] Custom supervisor script (#89)

--- snap/local/start-mysqld.sh
+++ snap/local/start-mysqld.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-# For security measures, applications should not be run as sudo.
-# Execute mysqld_safe as the non-sudo user: snap-daemon
-# Note: group is set to root due to backups related intricacies.
-exec "${SNAP}/usr/bin/setpriv" \
-    --clear-groups \
-    --reuid snap_daemon \
-    --regid root \
-    -- \
-    "${SNAP}/usr/bin/mysqld_safe" \
-    --defaults-file="${SNAP_DATA}/etc/mysql/mysql.cnf"
+# This script is designed to be a "supervisor process" for MySQL Server.
+# Such process is needed for it to be able to restart upon config changes.
+# Ref: https://dev.mysql.com/doc/refman/8.0/en/restart.html
+
+export MYSQLD_PARENT_PID=$$
+export MYSQLD_RESTART_CODE=16
+
+while true; do
+    # For security measures, applications should not be run as sudo.
+    # Execute mysqld as the non-sudo user: snap-daemon
+    # Note: group is set to root due to backups related intricacies.
+    exec "${SNAP}/usr/bin/setpriv" \
+        --clear-groups \
+        --reuid snap_daemon \
+        --regid root \
+        -- \
+        "${SNAP}/usr/sbin/mysqld" --defaults-file="${SNAP_DATA}/etc/mysql/mysql.cnf" &
+
+    wait $!
+
+    EXIT_CODE=$?
+
+    if [ ${EXIT_CODE} -ne ${MYSQLD_RESTART_CODE} ]; then
+        echo "MySQL exited with code ${EXIT_CODE}."
+        break
+    fi
+done
