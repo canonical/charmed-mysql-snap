b214930034ea193d1c9c8098f93d9ba5de69a925
Juan Luis Cano Rodríguez <juan.rodriguez@canonical.com>
2026-03-25 18:35:56 +0100

    Fix permissions at the initialize script

--- snap/local/initialize-mysqld.sh
+++ snap/local/initialize-mysqld.sh
@@ -2,4 +2,11 @@
 
 set -eo pipefail  # Exit on error
 
-exec "${SNAP}/usr/sbin/mysqld" --defaults-file="${SNAP_DATA}/etc/mysql/mysql.cnf" --initialize "$@"
+# Run mysqld initialization as snap_daemon user (similar to start-mysqld.sh)
+# This ensures proper permissions from the start
+exec "${SNAP}/usr/bin/setpriv" \
+    --clear-groups \
+    --reuid snap_daemon \
+    --regid root \
+    -- \
+    "${SNAP}/usr/sbin/mysqld" --defaults-file="${SNAP_DATA}/etc/mysql/mysql.cnf" --initialize "$@"
